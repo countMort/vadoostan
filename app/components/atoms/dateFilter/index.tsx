88fc1dd9426338a752cb00d54c44c1a07f4e0330
ali <alimahmudi.alami@gmail.com>
2025-03-23 14:20:02 +0330

    handle verticaly scroll in date filter

--- app/components/atoms/dateFilter/index.tsx
+++ app/components/atoms/dateFilter/index.tsx
@@ -1,3 +1,4 @@
+'use client';
 import classNames from 'classnames';
 import classes from './style.module.scss';
 import Image from 'next/image';
@@ -5,22 +6,54 @@ import moment from 'moment-jalaali';
 import { Text } from '@mantine/core';
 import { colors } from '@/colors';
 import { ExperienceListContext } from '@/app/experience-list/provider';
-import { useContext } from 'react';
+import { useContext, useRef, WheelEventHandler } from 'react';
 moment.loadPersian({ usePersianDigits: true, dialect: 'persian-modern' });
 
 const DateFilter = () => {
+  const scrollableDivRef = useRef<HTMLDivElement>(null);
   const { selectedDate, onSelectDate } = useContext(ExperienceListContext);
   const today = moment();
   const dates = [...new Array(30)].map((_, index) => {
     return today.clone().add(index, 'day');
   });
 
+  const scrollLeft = () => {
+    if (scrollableDivRef.current) {
+      scrollableDivRef.current.scrollTo({
+        left: scrollableDivRef.current.scrollLeft - 150,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollableDivRef.current) {
+      scrollableDivRef.current.scrollTo({
+        left: scrollableDivRef.current.scrollLeft + 150,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const handleWheel: WheelEventHandler<HTMLDivElement> = (event) => {
+    if (scrollableDivRef.current) {
+      scrollableDivRef.current.scrollLeft += event.deltaY;
+    }
+  };
+
   return (
     <div className={classes['wrapper']}>
-      <div className={classNames(classes['arrow'], classes['arrow--right'])}>
+      <div
+        onClick={scrollRight}
+        className={classNames(classes['arrow'], classes['arrow--right'])}
+      >
         <Image src={'/arrow.svg'} width={5} height={8} alt='arrow' />
       </div>
-      <div className={classes['list']}>
+      <div
+        onWheel={handleWheel}
+        ref={scrollableDivRef}
+        className={classes['list']}
+      >
         {dates.map((date, index) => {
           const isSelected =
             date.format('YYYY/MM/DD') === selectedDate?.format('YYYY/MM/DD');
@@ -54,7 +87,10 @@ const DateFilter = () => {
           );
         })}
       </div>
-      <div className={classNames(classes['arrow'], classes['arrow--left'])}>
+      <div
+        onClick={scrollLeft}
+        className={classNames(classes['arrow'], classes['arrow--left'])}
+      >
         <Image
           src={'/arrow.svg'}
           width={5}
